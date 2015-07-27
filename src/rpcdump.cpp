26d0bfe634c1cd07ef019be8af727c00469457dc
Extrememist <jacobms109@gmail.com>
2015-07-27 19:49:36 +1200

    Woops i did it again

--- src/rpcdump.cpp
+++ src/rpcdump.cpp
@@ -110,7 +110,7 @@ Value importprivkey(const Array& params, bool fHelp)
 {
     if (fHelp || params.size() < 1 || params.size() > 2)
         throw runtime_error(
-            "importprivkey <sembrosprivkey> [label]\n"
+            "importprivkey <sembroscoinprivkey> [label]\n"
             "Adds a private key (as returned by dumpprivkey) to your wallet.");
 
     string strSecret = params[0].get_str();
@@ -234,15 +234,15 @@ Value dumpprivkey(const Array& params, bool fHelp)
 {
     if (fHelp || params.size() != 1)
         throw runtime_error(
-            "dumpprivkey <sembrosaddress>\n"
-            "Reveals the private key corresponding to <sembrosaddress>.");
+            "dumpprivkey <sembroscoinaddress>\n"
+            "Reveals the private key corresponding to <sembroscoinaddress>.");
 
     EnsureWalletIsUnlocked();
 
     string strAddress = params[0].get_str();
     CBitcoinAddress address;
     if (!address.SetString(strAddress))
-        throw JSONRPCError(RPC_INVALID_ADDRESS_OR_KEY, "Invalid Sembros address");
+        throw JSONRPCError(RPC_INVALID_ADDRESS_OR_KEY, "Invalid SembrosCoin address");
     if (fWalletUnlockMintOnly) // ppcoin: no dumpprivkey in mint-only mode
         throw JSONRPCError(RPC_WALLET_UNLOCK_NEEDED, "Wallet is unlocked for minting only.");
     CKeyID keyID;
