f26ec6dc0f68352455aaea62f429cfe84b5f4add
Infernoman <jjaauto@hotmail.com>
2015-07-29 17:06:35 -0400

    test fix

--- src/net.cpp
+++ src/net.cpp
@@ -679,8 +679,8 @@ void ThreadSocketHandler2(void* parg)
             BOOST_FOREACH(CNode* pnode, vNodesCopy)
             {
                 if (pnode->fDisconnect ||
-                    (pnode->GetRefCount() <= 0 && pnode->vRecv.empty() && pnode->vSend.empty()) ||
-                    (PROTOCOL_VERSION < NOBLKS_VERSION_START || PROTOCOL_VERSION >= NOBLKS_VERSION_END) ) {
+                    (pnode->GetRefCount() <= 0 && pnode->vRecv.empty() && pnode->vSend.empty()))
+                {
                     // remove from vNodes
                     vNodes.erase(remove(vNodes.begin(), vNodes.end(), pnode), vNodes.end());
 
